c9b9e21b83ce86de2074d18524ef45f3cb3453a8
Anastasiia Sergienko <46891819+AnastasiiaSergienko@users.noreply.github.com>
2021-09-14 12:27:34 +0200

    Refactoring/198 documented jira credentials (#199)
    * #198: Documented how to provide Jira credentials.

--- doc/changes/changes_0.7.1.md
+++ doc/changes/changes_0.7.1.md
@@ -1,6 +1,6 @@
-# Exasol Release Droid 0.7.1, released 2021-??-??
+# Exasol Release Droid 0.7.1, released 2021-09-14
 
-Code name:
+Code name: Fixed problems with Jira release
 
 ## Summary
 
@@ -9,8 +9,13 @@ Code name:
 * #192: Made the config file optional for Jira release.
 * #194: Enabled creating a report directory if missing.
 
+## Documentation
+
+* #198: Documented how to provide Jira credentials.
+
 ## Refactoring
 
 * #189: Fixed code smells.
+* #197: Replaced 'robot' with 'droid' in tests.
 
 ## Dependency Updates
--- src/test/java/com/exasol/releasedroid/formatting/ChecksumFormatterTest.java
+++ src/test/java/com/exasol/releasedroid/formatting/ChecksumFormatterTest.java
@@ -10,22 +10,22 @@ import org.junit.jupiter.api.Test;
 class ChecksumFormatterTest {
     @Test
     void testCreateChecksumMap() {
-        final String checksum = "76f3e target/testing-release-robot-0.2.0.jar e031e "
-                + "target/testing-release-robot-0.2.0-sources.jar 1b37e target/testing-release-robot-0.2.0-javadoc.jar";
+        final String checksum = "76f3e target/testing-release-droid-0.2.0.jar e031e "
+                + "target/testing-release-droid-0.2.0-sources.jar 1b37e target/testing-release-droid-0.2.0-javadoc.jar";
         assertThat(ChecksumFormatter.createChecksumMap(checksum),
-                equalTo(Map.of("target/testing-release-robot-0.2.0.jar", "76f3e", //
-                        "target/testing-release-robot-0.2.0-sources.jar", "e031e", //
-                        "target/testing-release-robot-0.2.0-javadoc.jar", "1b37e")));
+                equalTo(Map.of("target/testing-release-droid-0.2.0.jar", "76f3e", //
+                        "target/testing-release-droid-0.2.0-sources.jar", "e031e", //
+                        "target/testing-release-droid-0.2.0-javadoc.jar", "1b37e")));
     }
 
     @Test
     void testCreateChecksumMapWithNewLineCharacters() {
-        final String checksum = "76f3e target/testing-release-robot-0.2.0.jar\n"
-                + "e031e  target/testing-release-robot-0.2.0-sources.jar\n"
-                + "45eb5  target/testing-release-robot-0.2.0-javadoc.jar\n";
+        final String checksum = "76f3e target/testing-release-droid-0.2.0.jar\n"
+                + "e031e  target/testing-release-droid-0.2.0-sources.jar\n"
+                + "45eb5  target/testing-release-droid-0.2.0-javadoc.jar\n";
         assertThat(ChecksumFormatter.createChecksumMap(checksum),
-                equalTo(Map.of("target/testing-release-robot-0.2.0.jar", "76f3e", //
-                        "target/testing-release-robot-0.2.0-sources.jar", "e031e", //
-                        "target/testing-release-robot-0.2.0-javadoc.jar", "45eb5")));
+                equalTo(Map.of("target/testing-release-droid-0.2.0.jar", "76f3e", //
+                        "target/testing-release-droid-0.2.0-sources.jar", "e031e", //
+                        "target/testing-release-droid-0.2.0-javadoc.jar", "45eb5")));
     }
 }
