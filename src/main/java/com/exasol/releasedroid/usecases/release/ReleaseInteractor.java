fe3c5ac2c50620546b1057af0cbc6a9bb8ec9586
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2021-10-06 16:19:45 +0200

    * #205: Suppressed a Sonar's warning

--- src/main/java/com/exasol/releasedroid/usecases/release/ReleaseInteractor.java
+++ src/main/java/com/exasol/releasedroid/usecases/release/ReleaseInteractor.java
@@ -64,6 +64,7 @@ public class ReleaseInteractor implements ReleaseUseCase {
         }
     }
 
+    // There is no a good workaround to avoid the second break here
     private List<Report> releaseOnPlatforms(final Repository repository, final List<PlatformName> platforms,
             final Set<PlatformName> skipValidationOn) {
         prepareRepositoryForRelease(repository);
