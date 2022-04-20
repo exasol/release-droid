36617a714c627b6d30328669d38366b95fcaf37b
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-04-20 13:28:29 +0200

    Implement review findings

--- src/test/java/com/exasol/releasedroid/main/RunnerIT.java
+++ src/test/java/com/exasol/releasedroid/main/RunnerIT.java
@@ -9,29 +9,27 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import com.exasol.mavenprojectversiongetter.MavenProjectVersionGetter;
 
-public class RunnerIT {
+class RunnerIT {
 
     @Test
-    void jarIsExecutable() throws IOException, InterruptedException {
-        String currentVersion = MavenProjectVersionGetter.getCurrentProjectVersion();
-        Path jar = Paths.get("target").resolve("release-droid-" + currentVersion + ".jar").toAbsolutePath();
+    void testJarIsExecutable() throws IOException, InterruptedException {
+        final String currentVersion = MavenProjectVersionGetter.getCurrentProjectVersion();
+        final Path jar = Paths.get("target").resolve("release-droid-" + currentVersion + ".jar").toAbsolutePath();
         if (!Files.exists(jar)) {
             fail("Jar " + jar + " does not exist. Run 'mvn package' before starting the integration tests");
         }
-        List<String> command = List.of("java", "-jar", jar.toString(), "--help");
-        String output = startCommand(command);
+        final String output = startCommand("java", "-jar", jar.toString(), "--help");
         assertThat(output, startsWith("usage: Release Droid"));
     }
 
-    private String startCommand(List<String> command) throws IOException, InterruptedException {
-        Process process = new ProcessBuilder(command).start();
-        int exitCode = process.waitFor();
+    private String startCommand(final String... command) throws IOException, InterruptedException {
+        final Process process = new ProcessBuilder(command).start();
+        final int exitCode = process.waitFor();
         if (exitCode != 0) {
             fail("Process failed with exit code " + exitCode + ", std error '" + readString(process.getErrorStream())
                     + "', std output '" + readString(process.getInputStream()) + "'.");
@@ -40,7 +38,7 @@ public class RunnerIT {
         return readString(process.getInputStream());
     }
 
-    private String readString(InputStream stream) throws IOException {
+    private String readString(final InputStream stream) throws IOException {
         return new String(stream.readAllBytes(), StandardCharsets.UTF_8);
     }
 }
