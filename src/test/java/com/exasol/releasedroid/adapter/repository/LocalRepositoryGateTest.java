c12bfc97d825987cfaf4feca208df3f08d306afa
KK <christoph.kuhnke@exasol.com>
2022-11-16 16:07:41 +0100

    increased test coverage

--- src/test/java/com/exasol/releasedroid/adapter/repository/LocalRepositoryGateTest.java
+++ src/test/java/com/exasol/releasedroid/adapter/repository/LocalRepositoryGateTest.java
@@ -2,18 +2,22 @@ package com.exasol.releasedroid.adapter.repository;
 
 import static org.eclipse.jgit.lib.Constants.R_TAGS;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 
 import org.eclipse.jgit.lib.Ref;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.Mockito;
 
+import com.exasol.releasedroid.usecases.exception.RepositoryException;
 import com.exasol.releasedroid.usecases.repository.version.Version;
 
 class LocalRepositoryGateTest {
@@ -33,6 +37,46 @@ class LocalRepositoryGateTest {
     void emptyRefs() {
         final Optional<Version> actual = LocalRepositoryGate.latestTagFromRefs(List.of());
         assertThat(actual.isPresent(), is(false));
+    }
+
+    @Test
+    void from(@TempDir final Path temp) throws Exception {
+        final String configuration = "[remote \"origin\"]\n" //
+                + "url = https://github.com/owner/full-name.git\n";
+        RemoteNameTest.emulateGitProject(temp, configuration);
+        final LocalRepositoryGate testee = LocalRepositoryGate.from(temp);
+        assertThat(testee.getName(), equalTo("owner/full-name"));
+    }
+
+    @Test
+    void fileContentSuccess(@TempDir final Path temp) throws Exception {
+        final String filename = "file.txt";
+        final String content = "sample content";
+        Files.writeString(temp.resolve(filename), content);
+        final LocalRepositoryGate testee = new LocalRepositoryGate(temp.toString(), "");
+        assertThat(testee.getSingleFileContentAsString(filename), equalTo(content));
+    }
+
+    @Test
+    void fileContentFailure() throws Exception {
+        final LocalRepositoryGate testee = new LocalRepositoryGate("/non/existing/folder", "");
+        final Exception e = assertThrows(RepositoryException.class,
+                () -> testee.getSingleFileContentAsString("file.txt"));
+        assertThat(e.getMessage(), startsWith("E-RD-REP-1: Cannot read a file from the local repository"));
+    }
+
+    @Test
+    void branchNameSuccess(@TempDir final Path temp) throws Exception {
+        RemoteNameTest.emulateGitProject(temp, "");
+        final LocalRepositoryGate testee = new LocalRepositoryGate(temp.toString(), "");
+        assertThat(testee.getBranchName(), equalTo("refactor/380-get-project-name"));
+    }
+
+    @Test
+    void branchNameFailure() {
+        final LocalRepositoryGate testee = new LocalRepositoryGate("/non/existing/folder", "");
+        final Exception e = assertThrows(RepositoryException.class, () -> testee.getBranchName());
+        assertThat(e.getMessage(), equalTo("E-RD-REP-6: Cannot retrieve a name of a local git branch."));
     }
 
     private Ref mockRef(final String name) {
--- src/test/java/com/exasol/releasedroid/adapter/repository/RemoteNameTest.java
+++ src/test/java/com/exasol/releasedroid/adapter/repository/RemoteNameTest.java
@@ -7,7 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -19,6 +22,7 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 class RemoteNameTest {
 
@@ -44,6 +48,16 @@ class RemoteNameTest {
         assertThat(testee.retrieve().get(), equalTo("owner/full-name"));
     }
 
+    @Test
+    void itest(@TempDir final Path temp) throws Exception {
+        final String configuration = "[remote \"origin\"]\n" //
+                + "url = https://github.com/owner/full-name.git\n";
+        emulateGitProject(temp, configuration);
+        final RemoteName testee = new RemoteName(Git.open(temp.toFile()));
+        assertThat(testee.retrieve().isPresent(), is(true));
+        assertThat(testee.retrieve().get(), equalTo("owner/full-name"));
+    }
+
     private Git git(final RemoteListCommand command) {
         final Git git = mock(Git.class);
         when(git.remoteList()).thenReturn(command);
@@ -64,5 +78,13 @@ class RemoteNameTest {
         } catch (final URISyntaxException exception) {
             throw new IllegalStateException(exception);
         }
+    }
+
+    static void emulateGitProject(final Path folder, final String configuration) throws IOException {
+        Files.createDirectories(folder.resolve(".git/refs"));
+        Files.createDirectories(folder.resolve(".git/objects"));
+        Files.writeString(folder.resolve(".git/HEAD"), "ref: refs/heads/refactor/380-get-project-name");
+        final Path configFile = folder.resolve(".git/config");
+        Files.writeString(configFile, configuration);
     }
 }
