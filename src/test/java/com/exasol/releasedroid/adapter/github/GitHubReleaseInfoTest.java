c8579a180555e18bf38a191f624d163197508b68
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-06-11 13:14:02 +0200

    Fix compile error

--- src/test/java/com/exasol/releasedroid/adapter/github/GitHubReleaseInfoTest.java
+++ src/test/java/com/exasol/releasedroid/adapter/github/GitHubReleaseInfoTest.java
@@ -4,9 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.net.*;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
